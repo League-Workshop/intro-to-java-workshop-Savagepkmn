6ed67cfe2235a2d5e1723d62cc84ac75641a06af
League Student <league@iMac-27.attlocal.net>
2019-07-31 14:22:16 -0700

    day 3

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,25 +7,25 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+			JOptionPane.showMessageDialog(null, " ")
 		// Get the user to enter an adjective
-
-		// Get the user to enter a type of liquid
-
+			String adjective = JOptionPane.showInputDialog(null, "write an adjective");
+					 // Get the user to enter a type of liquid
+			String liquid = JOptionPane.showInputDialog(null, "write a liquid");
 		// Get the user to enter a body part
-
+			String bodyPart = JOptionPane.showInputDialog("write a body part");
 		// Get the user to enter a verb
-
+					String verb = JOptionPane.showInputDialog("write a verb");
 		// Get the user to enter a place
-
+					String place = JOptionPane.showInputDialog("write a place");
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
 		// likely take a bite out of your [body part] if you [verb]. Whatever
 		// you do, if you have an open wound, try to find another way to get
 		// back to the [place]. Good luck!
-		
-		// Make a pop-up for the final story. You can use \n to go to the next line
+				
+				// Make a pop-up for the final story. You can use \n to go to the next line
 		
 
 	}
