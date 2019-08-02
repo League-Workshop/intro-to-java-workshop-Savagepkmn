e05a8dbde5e17d24481a9729377d8507d3dd8b2e
League Student <league@iMac-27.attlocal.net>
2019-08-02 14:19:08 -0700

    day 5.

--- src/section3/CandyCrush.java
+++ src/section3/CandyCrush.java
@@ -10,9 +10,9 @@ public class CandyCrush {
 		// 2. Change the next line so that the pop-up will tell them they have a crush on that person
 		JOptionPane.showMessageDialog(null, "You totally have a crush on "+name );
 		// 3. Ask the user for the name of their best friend
-		String friend = JOptionPane.showInputDialog(null, "Who's your best friend?")
+		String friend = JOptionPane.showInputDialog(null, "Who's your best friend?");
 		// 4. Tell them their best friend is as sweet as candy
-				JOptionPane.showMessageDialog(null, friend + "is as sweet as candy.")
+				JOptionPane.showMessageDialog(null, friend + "is as sweet as candy.");
 	} 
 }
 
